bd4b4257031d49ed22c29dc2439bdb154dd455d1
Soroush Mirzaee <DeltaTheM2@users.noreply.github.com>
2023-10-29 12:45:44 -0700

    v1

--- src/components/videoDisplay/VideoDisplay.tsx
+++ src/components/videoDisplay/VideoDisplay.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './../../pages/CourseDashboard/index.css';
 
 const VideoDisplay = ({ keywords }) => {
-  const generateVideoSrc = (keywords) => {
-    // Replace with your actual video generation logic
-    // Here is an example of generating a YouTube search URL
-    return `https://www.youtube.com/results?search_query=${encodeURIComponent(keywords.join(', '))}`;
-  };
+  const [videoId, setVideoId] = useState(null);
 
-  return (
+  useEffect(() => {
+    const fetchVideo = async () => {
+      const searchQuery = encodeURIComponent(keywords.join(', '));
+      const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&key=YOUR_YOUTUBE_DATA_API_KEY`);
+      const data = await response.json();
+      const videoId = data.items[0]?.id.videoId;
+      setVideoId(videoId);
+    };
+
+    if (keywords.length > 0) {
+      fetchVideo();
+    }
+  }, [keywords]);
+
+  return videoId ? (
     <iframe
       title="Lesson Video"
-      src={generateVideoSrc(keywords)}
+      src={`https://www.youtube.com/embed/${videoId}`}
       width="100%"
-      height="100%"
+      height="500px" // You can set this to whatever height you desire
+      frameBorder="0"
+      allowFullScreen
     />
+  ) : (
+    <p>Loading video...</p>
   );
 };
 

b14225eefe964fce994ee81c5d9c2cd088208401
Soroush Mirzaee <DeltaTheM2@users.noreply.github.com>
2023-10-31 23:55:56 -0700

    Json Parsing Update

--- src/components/videoDisplay/VideoDisplay.tsx
+++ src/components/videoDisplay/VideoDisplay.tsx
@@ -7,7 +7,7 @@ const VideoDisplay = ({ keywords }) => {
   useEffect(() => {
     const fetchVideo = async () => {
       const searchQuery = encodeURIComponent(keywords.join(', '));
-      const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&key=YOUR_YOUTUBE_DATA_API_KEY`);
+      const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&key=AIzaSyDiIynJqBlpeVWFBbf-uV2IhDZ2l7O7LnQ`);
       const data = await response.json();
       const videoId = data.items[0]?.id.videoId;
       setVideoId(videoId);
